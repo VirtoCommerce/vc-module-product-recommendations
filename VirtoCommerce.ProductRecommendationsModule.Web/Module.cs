7d7bc12635b0544978a765791fd60cfb010e55cb
Aleksandr Vishnyakov <av@virtoway.com>
2017-03-23 13:30:05 +0500

    Module settings values now may be specified for Store. #784

--- VirtoCommerce.ProductRecommendationsModule.Web/Module.cs
+++ VirtoCommerce.ProductRecommendationsModule.Web/Module.cs
@@ -1,8 +1,31 @@
-﻿using VirtoCommerce.Platform.Core.Modularity;
+﻿using System.Linq;
+using Microsoft.Practices.Unity;
+using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.ProductRecommendationsModule.Web
 {
     public class Module : ModuleBase
     {
+
+        private readonly IUnityContainer _container;
+
+        public Module(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public override void Initialize()
+        {
+        }
+
+        public override void PostInitialize()
+        {
+            base.PostInitialize();
+
+            var settingManager = _container.Resolve<ISettingsManager>();
+            var productRecommendationsSettings = settingManager.GetModuleSettings("VirtoCommerce.ProductRecommendations").ToArray();
+            settingManager.RegisterModuleSettings("VirtoCommerce.Store", productRecommendationsSettings);
+        }
     }
 }
