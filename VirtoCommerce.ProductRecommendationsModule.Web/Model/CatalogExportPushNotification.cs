bea8acb70d5d33ed2e6a1876bf78467eff11460e
Aleksandr Vishnyakov <av@virtoway.com>
2017-03-24 12:18:58 +0500

    Export current catalog into csv file prepared for Microsoft Recommendations API #785

---
+++ VirtoCommerce.ProductRecommendationsModule.Web/Model/CatalogExportPushNotification.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VirtoCommerce.Platform.Core.PushNotifications;
+
+namespace VirtoCommerce.ProductRecommendationsModule.Web.Model
+{
+    public class CatalogExportPushNotification : PushNotification
+    {
+        public CatalogExportPushNotification(string creator) : base(creator)
+        {
+            NotifyType = "CatalogCvsExportPrepatedForRecommendations";
+            Errors = new List<string>();
+        }
+
+        public string DownloadUrl { get; set; }
+
+        public DateTime? Finished { get; set; }
+
+        public long TotalCount { get; set; }
+
+        public long ProcessedCount { get; set; }
+
+        public long ErrorCount
+        {
+            get
+            {
+                return Errors != null ? Errors.Count() : 0;
+            }
+        }
+
+        public ICollection<string> Errors { get; set; }
+    }
+}
