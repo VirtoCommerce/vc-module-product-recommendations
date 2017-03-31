4a7354415e242485f57251300b08ea1bf5c1d376
Aleksandr Vishnyakov <av@virtoway.com>
2017-03-31 13:44:10 +0500

    Fixed bug with usage event class map

--- VirtoCommerce.ProductRecommendationsModule.Web/Export/CsvUsageEventMap.cs
+++ VirtoCommerce.ProductRecommendationsModule.Web/Export/CsvUsageEventMap.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using CsvHelper.Configuration;
-using VirtoCommerce.ProductRecommendationsModule.Core.Model;
 
 namespace VirtoCommerce.ProductRecommendationsModule.Web.Export
 {
     [CLSCompliant(false)]
-    public class CsvUsageEventMap : CsvClassMap<UsageEvent>
+    public class CsvUsageEventMap : CsvClassMap<CsvUsageEvent>
     {
         public CsvUsageEventMap()
         {
